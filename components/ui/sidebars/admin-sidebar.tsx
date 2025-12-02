50d6352c07831a5a3452e37f7428d1372891b258
Epreen Njolomole <epreennjolomole451@gmail.com>
2025-12-02 22:21:10 +0200

    Phase 2: Cleanup: Part 12

--- components/ui/sidebars/admin-sidebar.tsx
+++ components/ui/sidebars/admin-sidebar.tsx
@@ -1,42 +1,42 @@
-'use client'
+// 'use client'
 
-import { usePathname } from "next/navigation"
-import { HomeIcon, ShieldCheckIcon, StoreIcon, TicketPercentIcon } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import {assets} from "@/data/assets";
+// import { usePathname } from "next/navigation"
+// import { HomeIcon, ShieldCheckIcon, StoreIcon, TicketPercentIcon } from "lucide-react"
+// import Image from "next/image"
+// import Link from "next/link"
+// import {assets} from "@/data/assets";
 
-const AdminSidebar = () => {
+// const AdminSidebar = () => {
 
-    const pathname = usePathname()
+//     const pathname = usePathname()
 
-    const sidebarLinks = [
-        { name: 'Dashboard', href: '/admin', icon: HomeIcon },
-        { name: 'Stores', href: '/admin/stores', icon: StoreIcon },
-        { name: 'Approve Store', href: '/admin/approve', icon: ShieldCheckIcon },
-        { name: 'Coupons', href: '/admin/coupons', icon: TicketPercentIcon  },
-    ]
+//     const sidebarLinks = [
+//         { name: 'Dashboard', href: '/admin', icon: HomeIcon },
+//         { name: 'Stores', href: '/admin/stores', icon: StoreIcon },
+//         { name: 'Approve Store', href: '/admin/approve', icon: ShieldCheckIcon },
+//         { name: 'Coupons', href: '/admin/coupons', icon: TicketPercentIcon  },
+//     ]
 
-    return (
-        <div className="inline-flex h-full flex-col gap-5 border-r border-foreground/10 sm:min-w-60">
-            <div className="flex flex-col gap-3 justify-center items-center pt-8 max-sm:hidden">
-                <Image className="w-14 h-14 rounded-full" src={assets.gs_logo} alt="" width={80} height={80} />
-                <p className="text-foreground">Hi, GreatStack</p>
-            </div>
+//     return (
+//         <div className="inline-flex h-full flex-col gap-5 border-r border-foreground/10 sm:min-w-60">
+//             <div className="flex flex-col gap-3 justify-center items-center pt-8 max-sm:hidden">
+//                 <Image className="w-14 h-14 rounded-full" src={assets.gs_logo} alt="" width={80} height={80} />
+//                 <p className="text-foreground">Hi, GreatStack</p>
+//             </div>
 
-            <div className="max-sm:mt-6">
-                {
-                    sidebarLinks.map((link, index) => (
-                        <Link key={index} href={link.href} className={`relative flex items-center gap-3 text-foreground/60 hover:bg-foreground/10 p-2.5 transition ${pathname === link.href && 'bg-foreground/10 sm:text-foreground/60'}`}>
-                            <link.icon size={18} className="sm:ml-5" />
-                            <p className="max-sm:hidden">{link.name}</p>
-                            {pathname === link.href && <span className="absolute bg-primary dark:bg-secondary text-primary dark:text-secondary right-0 top-1.5 bottom-1.5 w-1 sm:w-1.5 rounded-l"></span>}
-                        </Link>
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+//             <div className="max-sm:mt-6">
+//                 {
+//                     sidebarLinks.map((link, index) => (
+//                         <Link key={index} href={link.href} className={`relative flex items-center gap-3 text-foreground/60 hover:bg-foreground/10 p-2.5 transition ${pathname === link.href && 'bg-foreground/10 sm:text-foreground/60'}`}>
+//                             <link.icon size={18} className="sm:ml-5" />
+//                             <p className="max-sm:hidden">{link.name}</p>
+//                             {pathname === link.href && <span className="absolute bg-primary dark:bg-secondary text-primary dark:text-secondary right-0 top-1.5 bottom-1.5 w-1 sm:w-1.5 rounded-l"></span>}
+//                         </Link>
+//                     ))
+//                 }
+//             </div>
+//         </div>
+//     )
+// }
 
-export default AdminSidebar
+// export default AdminSidebar
